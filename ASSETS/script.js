2d8a76f5a93aad251174b23c3af2deffb524685a
Maryan Abdalla <113936470+maryanne3@users.noreply.github.com>
2022-10-25 16:17:14 +0100

    restart script.js

--- ASSETS/script.js
+++ ASSETS/script.js
@@ -1,9 +1,8 @@
-var quizContainer = document.getElementById("container");
+// begin button
+function beginButton = document.getElementById("begin-button")
+          console.log('beginquiz');
 
-var  questionCount;
-const scoreCount = 0;
-const count = 5;
-var  countdown;
+
 
 // Quiz Questions & Arrays
 
@@ -30,110 +29,15 @@ const Questions = [
     options: ["//Comment here", "/*Comment here*/", "<!--Comment here-->", "'Comment here'"],
     correct: "//Comment here",
 
-},
-];
-var countOfQuestion = document.querySelector(".number-of-question");
-function quizCreator() {
-    Questions.sort(() => Math.random() - 0.5);
-
-    for (let i of Questions) {
-      i.options.sort(() => Math.random() - 0.5);
-    
-    let div = document.createElement("div");
-      div.classList.add("container-mid", "hide");
-      
-      countOfQuestion.innerHTML = 1 + " of " + Questions.length + " Question";
-      
-    let question_DIV = document.createElement("p");
-      question_DIV.classList.add("question");
-      question_DIV.innerHTML = i.question;
-      div.appendChild(question_DIV);
-    
-      div.innerHTML += `
-      <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
-       <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
-        <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
-         <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
-      `;
-      quizContainer.appendChild(div);
-    }
 }
+]
+
+      
+
+    
 
 
-var displayContainer = document.getElementById("display-container");
-const quizDisplay = (questionCount) => {
-    let quizCards = document.querySelectorAll(".container-mid")
-    quizCards.forEach((card) => {
-      card.classList.add("hide");
-    });
-    quizCards[questionCount].classList.remove("hide");
-  };
+
+  
 
 
-clearInterval(countdown);
-  options.forEach((element) => {
-    element.disabled = true;
-  });
-
-var scoreContainer = document.querySelector(".score-container");
-  function initial() {
-    quizContainer.innerHTML = "";
-    questionCount = 0;
-    scoreCount = 0;
-    count = 5;
-    clearInterval(countdown);
-    timerDisplay();
-    quizCreator();
-    quizDisplay(questionCount)
-  }
-
-// Next Question Button 
-var nextBtn = document.getElementById("next-button");
-nextBtn.addEventListener(
-    "click",
-    (displayNext = () => {
-        questionCount += 1;
-if (questionCount == Questions.length) {
-        displayContainer.classList.add("hide");
-        scoreContainer.classList.remove("hide");
-        
-// Player Scorel
-var userScore = document.getElementById("user-score");
-        userScore.innerHTML =
-          "Your score is " + scoreCount + " out of " + questionCount;
-      } else {
-        countOfQuestion.innerHTML =
-          questionCount + 1 + " of " + Questions.length + " Question";
-      quizDisplay(questionCount);
-      count = 5;
-      clearInterval(countdown);
-      QuizTimer();
-      }
-    })
-  );
-
-// Quiz Timer
-var timeRemaining = document.querySelector(".time-remaining");
-const QuizTimer = () => {
-    countdown = setInterval(() => {
-      count--;
-      timeRemaining.innerHTML = `${count}s`;
-      if (count == 0) {
-        clearInterval(countdown);
-        displayNext();
-      }
-    }, 10);
-  };
-
-// begin button
-var beginButton = document.getElementById("begin-button");
-beginButton.addEventListener("click", () => {
-    beginQuiz.classList.add("hide");
-    displayContainer.classList.remove("hide");
-    initial();
-  });
-var beginQuiz = document.querySelector(".begin-quiz");
-  window.onload = () => {
-    beginQuiz.classList.remove("hide");
-    displayContainer.classList.add("hide");
-  }
